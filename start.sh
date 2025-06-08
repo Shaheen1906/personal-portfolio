a69a99d3e12ae209641cdfcb214ee2ade9a47f77
shaheen <shahin.foslipy@gmail.com>
2025-06-08 22:51:25 +0530

    update sh

--- start.sh
+++ start.sh
@@ -12,6 +12,22 @@ python manage.py migrate --noinput
 # Collect static files
 python manage.py collectstatic --noinput
 
+
+# start.sh
+
+# Run one-time command
+python manage.py createsuperuser --noinput \
+  --username shaheenansari1906@gmail.com \
+  --email shaheenansari1906@gmail.com
+
+echo "from django.contrib.auth import get_user_model; \
+  User = get_user_model(); \
+  user = User.objects.get(username='shaheenansari1906@gmail.com'); \
+  user.set_password('adminpass@123'); \
+  user.save()" \
+  | python manage.py shell
+
+
 # Optional: create superuser (only runs if user doesn’t exist)
 # echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')" | python manage.py shell
 
