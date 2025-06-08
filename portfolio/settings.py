707f097c7d84704e9ca5dc0068ba2d7c955a9cea
shaheen <shahin.foslipy@gmail.com>
2025-06-08 21:39:28 +0530

    personal site

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -85,7 +85,9 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')  
+    )
 }
 
 # Password validation
